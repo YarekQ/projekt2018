ff560dcee80dfc361f3d2e571123c33a4baad45c
YarekQ <39624776+YarekQ@users.noreply.github.com>
2018-05-30 12:59:53 +0200

    druga wersja

---
+++ dzien2/nowy20180528.py
@@ -0,0 +1,15 @@
+x=int(input("podaj pierwszą liczbę: "))
+y=int(input("podaj drugą liczbę: "))
+print("x+y=",x+y)
+
+#formatowanie stringów
+print(f"suma liczb {x} i {y} wynosi",x+y)
+print("tsts",end='')
+print("tsts",end="")
+print("koniecs",end="")
+
+zdaniekolejne=input('podaj teksta:')
+if "a" in zdaniekolejne:
+    print ("a jest")
+else:
+    print("nie ma")
