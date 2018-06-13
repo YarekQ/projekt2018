293123eec9722167c0a78ee27de2297213e84393
YarekQ <Yarek.Kurgan@gmail.com>
2018-06-13 14:38:01 +0200

    2018 06 13 01 update

---
+++ csvreader/wydruk.py
@@ -0,0 +1,36 @@
+def drukuj(database):
+    """
+    funkcja selektywnego przeglądu bazy danych
+    :param database:
+    :return: nic (wydruk)
+    """
+    underlyings=[]
+    securities=[]
+    exchanges=[]
+
+    for line in range(0, len(database)):
+        if database[line][0] not in underlyings:
+            underlyings.append(database[line][0])
+        if database[line][1] not in securities:
+            securities.append(database[line][1])
+        if database[line][11] not in exchanges:
+            exchanges.append(database[line][11])
+    print(f"znaleziono dane dla transakcji:\nunderlaying:{underlyings[1:]}\nsecurities: {securities[1:]}\nexchanges: {exchanges[1:]}\n")
+
+def filtruj(database,cos):
+    """
+    wydruk transakcji z zadanym parametrem
+    :param database:
+    :param cos: szukany string
+    :return: nic (wydruk)
+    """
+    # wydruk nagłówka
+    print(f"wydruk transakcji dla wartości filtra: {cos}\n")
+    print(f"{database[0][0]:14} {database[0][1]:14} {database[0][2]:16} {database[0][3]:8} {database[0][4]:8} {database[0][5]:8} {database[0][6]:8} {database[0][7]:8}\
+                    {database[0][8]:8} {database[0][9]:8} {database[0][10]:8} {database[0][11]:8} {database[0][12]:8} {database[0][13]:16} {database[0][14]:8}")
+
+    for line in range(0,len(database)):
+
+        if cos in database[line]:
+            print(f"{database[line][0]:14} {database[line][1]:14} {database[line][2]:16} {database[line][3]:8} {database[line][4]:8} {database[line][5]:8} {database[line][6]:8} {database[line][7]:8}\
+                    {database[line][8]:8} {database[line][9]:8} {database[line][10]:8} {database[line][11]:8} {database[line][12]:8} {database[line][13]:16} {database[line][14]:8}")
